cef7cd95e9ad15829033a42e6ba79252d66bd870
tmquan3005 <tmquan3005@gmail.com>
2019-06-22 12:12:50 +0700

    Update index.js, sua lai 2 file tag

--- src/index.js
+++ src/index.js
@@ -1,91 +1,4 @@
-// import {mxFirebase} from './mx';
-// import './mx.css';
-// import './index.css';
-
-
-
-// var firebaseConfig = {
-//     apiKey: "AIzaSyBLg1JzDcFwnF7e1PMGbgVW0ikqgXY3yw0",
-//     authDomain: "an-camp.firebaseapp.com",
-//     databaseURL: "https://an-camp.firebaseio.com",
-//     projectId: "an-camp",
-//     storageBucket: "an-camp.appspot.com",
-//     messagingSenderId: "523389688134",
-//     appId: "1:523389688134:web:1c752a2dbdae9a2c"
-// };
-
-
-// mxFirebase.init(firebaseConfig);
-
-
-// document.getElementById("sign-in-form").addEventListener("submit", async (e) => {
-//     e.preventDefault();
-//     const email = document.getElementById("email").value;
-//     const password = document.getElementById("password").value;
-//     try {
-//         await mxFirebase.signIn(email, password);
-//         console.log("login sucess");
-//     } catch(err) {
-//     document.getElementById("error-message").innerText = err.message;
-//     }
-
-// })
-
-// // // `import riot from 'riot';
-
-
-// // // import "./tags/homepage.tag";
-// // // import "./tags/signin.tag";
-
-// // // const homePage = riot.mount("div#root", "homepage")
-
-// // // import {mxFirebase} from './mx';
-// // // import './tags/mx.css';
-// // // import './tags/index.css';
-// // // import './tags/homepage.tag';
-// // // import riot from 'riot';
-// // // import { runInContext } from 'vm';
-// // // import "./tags/upload.tag";
-
-
-
-// // // var firebaseConfig = {
-// // //     apiKey: "AIzaSyBLg1JzDcFwnF7e1PMGbgVW0ikqgXY3yw0",
-// // //     authDomain: "an-camp.firebaseapp.com",
-// // //     databaseURL: "https://an-camp.firebaseio.com",
-// // //     projectId: "an-camp",
-// // //     storageBucket: "an-camp.appspot.com",
-// // //     messagingSenderId: "523389688134",
-// // //     appId: "1:523389688134:web:1c752a2dbdae9a2c"
-// // // };
-
-
-// // // mxFirebase.init(firebaseConfig);
-
-// // // document.getElementById("sign-in-form").addEventListener("submit", async (e) => {
-// // //     e.preventDefault();
-// // //     const email = document.getElementById("email").value;
-// // //     const password = document.getElementById("password").value;
-// // //     try {
-// // //         await mxFirebase.signIn(email, password);
-// // //         console.log("login sucess");
-// // //     } catch(err) {
-// // //     document.getElementById("error-message").innerText = err.m
-
-// // //     }
-// // // });
-
-// // // route('/home', async () => {
-// // //         const products = await nxFirebase.collection('products').getAll;
-// // //         console.log(products);
-// // //         const opts = {
-// // //             products: products
-// // //         }
-// // //         const homepage = riot.mount('root', 'homepage', opts);
-// // // });
-
-// // // route.start(true)
-
+import firebase from "firebase"
 import {mxFirebase} from './mx';
 import './mx.css';
 import './index.css';
@@ -110,22 +23,47 @@ mxFirebase.init(firebaseConfig);
 
 route.base("/")
 route("/signin", () => {
-    const signin = riot.mount("div#root", "signin");
-    document.getElementById("sign-up-form").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    try {
-        await mxFirebase.signUp(email, password);
-        console.log("login sucess");
-    } catch(err) {
-    document.getElementById("error-message").innerText = err.message;
-    }
+        const signin = riot.mount("div#root", "signin");
+        document.getElementById("sign-in-form").addEventListener("submit", async (e) => {
+        e.preventDefault();
+        console.log("submited")
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        try {
+            await mxFirebase.signIn(email, password);
+            console.log("login sucess");
+        } catch(err) {
+            document.getElementById("error-message").innerText = err.message;
+        }
     });
 })
 
-route("/signup",()=>{
-    const signup = riot.mount("div#root","signup")
+route("/signup",() => {
+    const signup = riot.mount("div#root","signup");
+    document.getElementById("sign-up-form").addEventListener("submit", async (e) => {
+        e.preventDefault();
+        console.log("submited")
+        const email = document.getElementById("email").value
+        const name = document.getElementById("name").value
+        const pass = document.getElementById("password").value
+        const pass_cnf = document.getElementById("password-cnf").value
+        console.log(email,pass)
+        if (pass == pass_cnf){
+            try{
+                await mxFirebase.signUp(email,pass)
+                firebase.auth().onAuthStateChanged(()=>{
+                    console.log(firebase.auth().currentUser.uid)
+                })
+            }
+            catch(err){
+                document.getElementById("error-message").textContent = err.message
+            }
+        }
+        else {
+            document.getElementById("error-message").textContent = "Password and password confirm are not match"
+        }
+    })
+    
 })
 
 route.start(true)
--- src/tags/signup.tag
+++ src/tags/signup.tag
@@ -6,23 +6,23 @@
         <form id="sign-up-form" action="">
             <div>
                 <label for="">Full Name</label>
-                <input class="fullwidth" type="email" required placeholder="Enter your full name">
+                <input class="fullwidth" id="name" type="text" required placeholder="Enter your full name">
             </div>
-        <div>
-            <label for="">Email Adress</label>
-            <input class="fullwidth" id="email" type="email" required placeholder="email fake@gmail.com">
-        </div>
-        <div>
-            <label for="">Password</label>
-            <input class="fullwidth" id="password" type="password" required placeholder="Password">
-        </div>
-        <div>
-            <label for="">Confirm Password</label>
-            <input class="fullwidth" id="password" type="password" required placeholder="Confirm Password">
-        </div>
-            <p id="error-message"></p>
-            <button class="fullwidth">Sign Up</button>
-        </div>
+            <div>
+                <label for="">Email Adress</label>
+                <input class="fullwidth" id="email" type="email" required placeholder="email fake@gmail.com">
+            </div>
+            <div>
+                <label for="">Password</label>
+                <input class="fullwidth" id="password" type="password" required placeholder="Password">
+            </div>
+            <div>
+                <label for="">Confirm Password</label>
+                <input class="fullwidth" id="password-cnf" type="password" required placeholder="Confirm Password">
+            </div>
+                <p id="error-message"></p>
+                <button class="fullwidth">Sign Up</button>
+            </div>
         </form>  
     </div>
 </signup>
